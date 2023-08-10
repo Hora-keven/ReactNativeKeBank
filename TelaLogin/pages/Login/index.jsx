a1143dd6e1f0c3dc315e149ec3b31601418b10b0
Hora-keven <123791537+Hora-keven@users.noreply.github.com>
2023-08-10 17:00:25 -0300

    Add files via upload

---
+++ TelaLogin/pages/Login/index.jsx
@@ -0,0 +1,34 @@
+import { View, Text, TextInput} from "react-native";
+import styles from "./styles";
+import {BsArrowRight} from "react-icons/bs"
+import { TouchableOpacity } from "react-native";
+
+export default function Login(){
+    return(
+        <View style={styles.container}>
+            
+        <View style={styles.nameSlogan}>
+            <View style={styles.title}>
+                <Text style={styles.txt}>Ke</Text>
+                <Text style={styles.secondPartName}>Bank</Text>
+            </View>
+        </View>
+        <View style={styles.inputs}>
+            <TextInput  style={styles.input} placeholder="Nome" />
+            <TextInput  style={styles.input} placeholder="Data de nascimento: "/>
+            <TextInput  style={styles.input} placeholder="Digite seu CPF: "/>
+            <TextInput  style={styles.input} placeholder="Digite seu E-mail: "/>
+            <TextInput  style={styles.input} placeholder="Crie sua senha: "/>
+            <TextInput  style={styles.input} placeholder="Repita a senha: "/>
+        </View>
+       <View>
+            <TouchableOpacity style={styles.button}>
+                <Text style={styles.Arrowbutton}><BsArrowRight/></Text>
+            </TouchableOpacity>
+       </View>
+     
+        <View style={styles.polign}></View>
+   
+        </View>
+    )
+}
---
+++ TelaLogin/pages/Login/loginolduser.jsx
@@ -0,0 +1,30 @@
+import { View, Text, TextInput} from "react-native";
+import styles from "./stylesUser";
+import {BsArrowRight} from "react-icons/bs"
+import { TouchableOpacity } from "react-native";
+
+export default function Login_user({navigation}){
+    return(
+        
+        <View style={styles.container}>
+            
+        <View style={styles.nameSlogan}>
+            <View style={styles.title}>
+                <Text style={styles.txt}>Ke</Text>
+                <Text style={styles.secondPartName}>Bank</Text>
+            </View>
+        </View>
+        <View style={styles.inputs}>
+          
+            <TextInput  style={styles.input} placeholder="Digite seu CPF: "/>
+            <TextInput  style={styles.input} placeholder="Digite sua senha: "/>
+        </View>
+       <View>
+            <TouchableOpacity style={styles.button}>
+                <Text style={styles.Arrowbutton}><BsArrowRight/></Text>
+            </TouchableOpacity>
+       </View>
+       <View style={styles.polign}></View>
+        </View>
+    )
+}
