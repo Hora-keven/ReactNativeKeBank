a154e56eb886d0975d3113029c5802285c548d88
Hora-keven <123791537+Hora-keven@users.noreply.github.com>
2023-08-10 16:57:54 -0300

    Update App.js

--- TelaLogin/App.js
+++ TelaLogin/App.js
@@ -1,20 +1,19 @@
-
 import {NavigationContainer} from '@react-navigation/native'
 import { createStackNavigator} from '@react-navigation/stack'
 import Home from './pages/Home'
 import Login from './pages/Login'
+import LoginUser from './pages/Login/loginolduser.jsx'
 
 const stack = createStackNavigator()
 export default function App() {
   return (
     <NavigationContainer>
       <stack.Navigator>
-        <stack.Screen name='home' component={Home}/>
-        <stack.Screen name='login' component={Login}/>
+        <stack.Screen  name='home' component={Home} options={{headerShown:false}}/>
+        <stack.Screen name='login' component={Login} options={{headerShown:false}}/>
+        <stack.Screen name='login_old_user' component={LoginUser} options={{headerShown:false}}/>
       </stack.Navigator>
 
     </NavigationContainer>
   );
 }
-
-
