a2a6b3bd48f3dad78c09d98949e96db30e598b69
Hora-keven <123791537+Hora-keven@users.noreply.github.com>
2023-11-17 11:28:31 -0300

    Add files via upload

---
+++ components/dropdownfees.jsx
@@ -0,0 +1,21 @@
+import {Dropdown} from "react-native-element-dropdown";
+import { ApiContext } from "../pages/context/APicontext";
+import { useContext } from "react";
+
+function DropdownFees(){
+    const {loan, changeLoan} = useContext(ApiContext)
+    
+    const options = [
+        {label:'12x', value:12},
+        {label:'24x', value:24},
+        {label:'36x', value:36},
+        
+    ]
+    return(
+    <Dropdown data={options} placeholder="Parcelas" placeholderStyle={{color:"white"}} value={loan} style={{width:100, color:"white"}} onChange={item=>{changeLoan(item.value)
+    console.log(item.value)}} labelField="label" 
+    valueField="value"/>
+    )
+}
+
+export default DropdownFees
